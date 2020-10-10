363536708d8ee42e97d97c81ff2fcf8bf192fc56
Soham Sarkar <72433693+soh-sarkar@users.noreply.github.com>
2020-10-10 23:59:36 +0530

    Update solution.c

--- HackerRank Problems/Bubble Sort/solution.c
+++ HackerRank Problems/Bubble Sort/solution.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 main(){
-    int n,i,j,count=0;
-    long int a[1000],tmp;
-    scanf("%d",&n);
-    for(i=0;i<n;i++){
-        scanf("%ld",&a[i]);
-    }              // 1,4,3,7,2
+    int n,i,j,count=0;  // datatypes given
+    long int a[1000],tmp;  
+    scanf("%d",&n); // reading values
+    for(i=0;i<n;i++){   // loops for traversing
+        scanf("%ld",&a[i]);  // reading values from loop
+    }           
     for(i=0;i<n;i++){
         for(j=0;j<n-1-i;j++){
-            if(a[j]>a[j+1]){
-               tmp=a[j];
+            if(a[j]>a[j+1]){  // condition for Bubble sort i.e if previous element is greater than the next element then swap
+               tmp=a[j];     // temporary variable for swaping
                a[j]=a[j+1];
-               a[j+1]=tmp;
-               count++; 
+               a[j+1]=tmp; // swaping done
+               count++; // counting the number of swaping required
             }
         }
     }
-    printf("Array is sorted in %d swaps.\n",count);
-    printf("First Element: %d\n",a[0]);
-    printf("Last Element: %d\n",a[n-1]);
+    printf("Array is sorted in %d swaps.\n",count); // printing the number of minimum steps required for Bubble sort the series of numbers
+    printf("First Element: %d\n",a[0]);  // printing first element
+    printf("Last Element: %d\n",a[n-1]); // printing last element
     return 0;
 }
